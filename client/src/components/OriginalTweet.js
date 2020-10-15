f059837a2fa0a042b086dc1bb7077486afce9870
Tori Miller <millertori04@gmail.com>
2020-10-14 20:55:52 -0700

    converted OriginalTweet from function to class

--- client/src/components/OriginalTweet.js
+++ client/src/components/OriginalTweet.js
@@ -1,69 +1,43 @@
 import React, { useState } from 'react';
 
-// export default class OriginalTweet extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             text: ''
-//         }
+export default class OriginalTweet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+            textArray: []
+        }
 
-//         this.handleText = this.handleText.bind(this);
-//     }
-
-
-//     handleText(e) {
-//         this.setState({
-//             text: e.target.value
-//         })
-//     }
-
-//     render() {
-//         {console.log('this.state:', this.state)}
-//         return (
-//             <div>
-//                 <input 
-//                     type="text"
-//                     value={this.state.text}
-//                     onChange={this.handleText}
-//                     />
-//             </div>
-//         )
-//     }
-// }
-
-export default function OriginalTweet() {
-    // useState will return an array, with the first item in it being the current 
-    // value of the state, and the second item being a setter to update that value.
-    const [text, handleText] = useState('');
-    const [textArray, setTextArray] = useState([]);
-
-    console.log('text:', text);
-    // console.log('textArray', textArray);
-
-
-//      Keep track of state for an input field
-//      Update the value on change
-//      Make the field value available to the submit handler
-{console.log('Original Tweet rendered')}
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        console.log('handleSubmit ran');
-        setTextArray([text])
-        /* need to figure out how to push 'text' to 'textArray' */
-        console.log('textArray:', textArray)
+        this.handleText = this.handleText.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('handleSubmit ran')
+        this.setState({textArray: this.state.text.split(' ')})
+    }
+
+
+    handleText(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    render() {
+        {console.log('this.state:', this.state)}
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
                 <input 
                     type="text"
-                    value={text}
-                    onChange={e => handleText(e.target.value)}
+                    value={this.state.text}
+                    onChange={this.handleText}
                     />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
+                    <button type="submit">Submit</button>
+                </form>      
+            </div>
+        )
+    }
 }
