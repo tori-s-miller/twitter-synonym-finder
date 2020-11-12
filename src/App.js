836782374131f737ec40a9c07328d1c54fdbf20a
Tori Miller <millertori04@gmail.com>
2020-11-11 21:46:03 -0800

    button click style and arrow transition

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default class App extends React.Component {
         adverbs: []
       },
       currentWordType: null,
-      clickedWord: null
+      clickedWord: null,
     }
   }
 
--- src/components/Synonyms.js
+++ src/components/Synonyms.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import arrow from '../img/arrow.svg';
 
 function ListItem(props) {
   const { value, clickedWord, handleInputClick } = props;
@@ -33,8 +34,7 @@ export default class Synonyms extends React.Component {
         this.state = {
           isLoaded: false,
           words: [],
-          clickedWord: '',
-          showListItems: false
+          clickedWord: ''
         }
     }
 
@@ -107,8 +107,10 @@ export default class Synonyms extends React.Component {
                 </div>
                 <div className="synonyms-sub-container">
                   <div className="nouns synonym-container">
+                    <img src={arrow} alt="arrow" className={this.props.currentWordType === 'nouns' ? 'arrow-active' : 'arrow'} />
                     <h2 onClick={this.setNouns} className={this.props.currentWordType === 'nouns' ? 'synonym-active' : 'synonym-inactive'}>NOUNS</h2>
                     <ul>
+                      <div className="list-item-container">
                       {this.props.currentWordType === 'nouns' && this.state.words.map((word, index) => (
                         word.tags !== undefined && word.tags.includes('n') && (
                           <div key={index} className="container">
@@ -117,9 +119,11 @@ export default class Synonyms extends React.Component {
                           )
                         )
                       )}
+                      </div>
                     </ul>
                   </div>
                   <div className="verbs synonym-container">
+                    <img src={arrow} alt="arrow" className={this.props.currentWordType === 'verbs' ? 'arrow-active' : 'arrow'} />
                     <h2 onClick={this.setVerbs} className={this.props.currentWordType === 'verbs' ? 'synonym-active' : 'synonym-inactive'}>VERBS</h2>
                     <ul>
                       {this.props.currentWordType === 'verbs' && this.state.words.map((word, index) => (
@@ -133,6 +137,7 @@ export default class Synonyms extends React.Component {
                     </ul>
                   </div>
                   <div className="adjectives synonym-container">
+                    <img src={arrow} alt="arrow" className={this.props.currentWordType === 'adjectives' ? 'arrow-active' : 'arrow'} />
                     <h2 onClick={this.setAdjectives} className={this.props.currentWordType === 'adjectives' ? 'synonym-active' : 'synonym-inactive'}>ADJECTIVES</h2>
                     <ul>
                     {this.props.currentWordType === 'adjectives' && this.state.words.map((word, index) => (
@@ -146,6 +151,7 @@ export default class Synonyms extends React.Component {
                     </ul>
                   </div>
                   <div className="adverbs synonym-container">
+                    <img src={arrow} alt="arrow" className={this.props.currentWordType === 'adverbs' ? 'arrow-active' : 'arrow'} />
                     <h2 onClick={this.setAdverbs} className={this.props.currentWordType === 'adverbs' ? 'synonym-active' : 'synonym-inactive'}>ADVERBS</h2>
                     {this.props.currentWordType === 'adverbs' && this.state.words.map((word, index) => (
                       word.tags !== undefined && word.tags.includes('adv') && (
